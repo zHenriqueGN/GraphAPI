5ddc397ccff57714c94f2ed6651547030a57871a
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-10-14 15:57:41 -0300

    feat: inject category db in resolver

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -13,7 +13,15 @@ import (
 
 // CreateCategory is the resolver for the createCategory field.
 func (r *mutationResolver) CreateCategory(ctx context.Context, input *model.NewCategory) (*model.Category, error) {
-	panic(fmt.Errorf("not implemented: CreateCategory - createCategory"))
+	category, err := r.CategoryDB.Create(input.Name, *input.Description)
+	if err != nil {
+		return nil, err
+	}
+	return &model.Category{
+		ID:          category.ID,
+		Name:        category.Name,
+		Description: &category.Description,
+	}, nil
 }
 
 // CreateCourse is the resolver for the createCourse field.
@@ -23,7 +31,21 @@ func (r *mutationResolver) CreateCourse(ctx context.Context, input *model.NewCou
 
 // Categories is the resolver for the categories field.
 func (r *queryResolver) Categories(ctx context.Context) ([]*model.Category, error) {
-	panic(fmt.Errorf("not implemented: Categories - categories"))
+	categories, err := r.CategoryDB.FindAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var categoriesModel []*model.Category
+	for _, category := range categories {
+		categoriesModel = append(categoriesModel, &model.Category{
+			ID:          category.ID,
+			Name:        category.Name,
+			Description: &category.Description,
+		})
+	}
+
+	return categoriesModel, nil
 }
 
 // Courser is the resolver for the courser field.
